94e39fc2448de8377d024b3356e8c3b844b576f6
Andrii Vasyliev <sol@hiqdev.com>
2016-08-19 16:27:56 +0000

    + params config file

---
+++ src/config/params.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'orgName'           => '',
+
+    'facebook_link'     => '',
+    'twitter_link'      => '',
+    'youtube_link'      => '',
+    'vk_link'           => '',
+    'instagram_link'    => '',
+    'pinterest_link'    => '',
+];
