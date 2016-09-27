51a993b59fc27b8831e60450275125caabb66637
tofid <andreyklochok@gmail.com>
2016-09-27 19:59:34 +0300

    Removed old `all` css and js files, reorganization theme asset

--- src/Asset.php
+++ src/Asset.php
@@ -1,6 +1,11 @@
 <?php
 
 namespace hiqdev\themes\dataserv;
+use hipanel\assets\LessSpaceAsset;
+use hiqdev\assets\pnotify\PNotifyAsset;
+use yii\bootstrap\BootstrapAsset;
+use yii\web\JqueryAsset;
+use yii\web\YiiAsset;
 
 /**
  * Theme main asset bundle.
@@ -18,23 +23,49 @@ class Asset extends \yii\web\AssetBundle
     public $css = [
         '//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css',
         '//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css',
-        YII_DEBUG ? 'css/all.css' : 'css/all.min.css',
+
+        'css/bootstrap-theme.min.css',
+        'css/block_grid_bootstrap.css',
+        'css/owl.carousel.css',
+        'css/owl.theme.css',
+        'css/animate-custom.css',
+        'css/slicknav.min.css',
+        'css/tablesaw.css',
+        'css/odometer-theme-default.css',
+        'css/style.css',
         'css/custom.css',
     ];
 
     public $js = [
-        '//ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js',
         '//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js',
-        YII_DEBUG ? 'js/all.js' : 'js/all.min.js',
-        '//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js',
+        'js/morphext.min.js',
+        'js/hoverIntent.js',
+        'js/superfish.min.js',
+        'js/owl.carousel.js',
+
+        'js/ripple-effect.js',
+        'js/wow.min.js',
+        'js/jquery.form.min.js',
+        'js/jquery.slicknav.min.js',
+        'js/retina.min.js',
+        'js/isotope.pkgd.min.js',
+        'js/tablesaw.js',
+        'js/tablesaw-init.js',
+        'js/responsiveEqualHeight.js',
+
+        'js/hisite.js',
+        'js/custom.js',
     ];
 
     /**
      * @inheritdoc
      */
     public $depends = [
-        \yii\web\YiiAsset::class,
-        \yii\web\JqueryAsset::class,
-        \yii\bootstrap\BootstrapAsset::class,
+        YiiAsset::class,
+        JqueryAsset::class,
+        BootstrapAsset::class,
+        LessSpaceAsset::class,
+        PNotifyAsset::class,
+
     ];
 }
