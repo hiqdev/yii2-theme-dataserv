26f2310aff7679f32bee2d949925e7381bbfa7dd
tofid <andreyklochok@gmail.com>
2016-10-06 15:47:28 +0300

    Added visual style to CartTeaser view

--- src/views/widgets/CartTeaser.php
+++ src/views/widgets/CartTeaser.php
@@ -1,6 +1,16 @@
+<?php
+
+/** @var object $cart */
+/** @var object $widget */
+
+$positionsCount = count($cart->positions);
+if ($positionsCount > 0) {
+    $this->registerCss("span.badge { background-color: #FF4081!important; }");
+}
+?>
 <div id="top-cart" class="pull-right nav">
     <a href="<?= $widget->module->createUrl() ?>" class="quick-nav">
         <i class="fa fa-shopping-cart"></i>
-        <span class="hidden-xs"><?= Yii::t('hisite', 'View Cart') ?> <span class="badge"><?= count($cart->positions) ?></span></span>
+        <span class="hidden-xs"><?= Yii::t('hisite', 'View Cart') ?> <span class="badge"><?= $positionsCount ?></span></span>
     </a>
 </div>
