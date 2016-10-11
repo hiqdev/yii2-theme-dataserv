75487ea673b881966599ddffd2283a7700a4d387
tofid <andreyklochok@gmail.com>
2016-10-11 18:03:32 +0300

    Fixed cart count css style

--- src/views/widgets/CartTeaser.php
+++ src/views/widgets/CartTeaser.php
@@ -5,12 +5,12 @@
 
 $positionsCount = count($cart->positions);
 if ($positionsCount > 0) {
-    $this->registerCss("span.badge { background-color: #FF4081!important; }");
+    $this->registerCss("#top-cart span.cart-count { background-color: #FF4081!important; padding: 0 5px;}");
 }
 ?>
 <div id="top-cart" class="pull-right nav">
     <a href="<?= $widget->module->createUrl() ?>" class="quick-nav">
         <i class="fa fa-shopping-cart"></i>
-        <span class="hidden-xs"><?= Yii::t('hisite', 'View Cart') ?> <span class="badge"><?= $positionsCount ?></span></span>
+        <span class="hidden-xs"><?= Yii::t('hisite', 'Cart') ?></span>&nbsp;&nbsp;<span class="cart-count"><?= $positionsCount ?></span>
     </a>
 </div>
