403ab34db86cd94f98e4c9397b1885a19e1cfd75
tofid <andreyklochok@gmail.com>
2016-10-19 16:38:48 +0300

    Fixed cart teaser css grey backgroud when the cart is epmty

--- src/views/widgets/CartTeaser.php
+++ src/views/widgets/CartTeaser.php
@@ -4,6 +4,7 @@
 /** @var object $widget */
 
 $positionsCount = count($cart->positions);
+$this->registerCss("#top-cart span.cart-count { background-color: #999; padding: 0 5px;}");
 if ($positionsCount > 0) {
     $this->registerCss("#top-cart span.cart-count { background-color: #FF4081!important; padding: 0 5px;}");
 }
