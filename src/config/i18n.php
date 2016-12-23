1d3ce353e1f8f2d2e28b9c61291770442079317c
tofid <andreyklochok@gmail.com>
2016-12-23 12:54:34 +0200

    Added i18n config

---
+++ src/config/i18n.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'sourcePath' => dirname(__DIR__),
+    'messagePath' => dirname(__DIR__) . '/messages',
+    'languages' => ['ru'],
+    'removeUnused' => true,
+    'markUnused' => true,
+    'sort' => true,
+];
