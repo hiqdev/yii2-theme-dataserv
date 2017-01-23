bd928d781c6210a2913d803ce8f88b35e4b18853
tofid <andreyklochok@gmail.com>
2017-01-23 18:46:42 +0200

    Added screenshot

--- src/Theme.php
+++ src/Theme.php
@@ -2,14 +2,14 @@
 
 namespace hiqdev\themes\dataserv;
 
-use Yii;
-
 /**
  * Theme class.
  */
 class Theme extends \hiqdev\thememanager\Theme
 {
     public $label = 'DataServ';
+
+    public $screenshot = '@hiqdev/themes/dataserv/assets/dataserv.png';
 
     public $assets = [
         Asset::class,
